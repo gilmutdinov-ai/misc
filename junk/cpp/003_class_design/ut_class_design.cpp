787c781bf3387db7b248b8cb7e40e25845d121f3
gilmutdinov-ai <artur@gilmutdinov.ai>
2024-09-30 09:25:37 +0300

    junk tests

---
+++ junk/cpp/003_class_design/ut_class_design.cpp
@@ -0,0 +1,10 @@
+#include <catch2/catch_test_macros.hpp>
+
+// Software must be adaptable to frequent changes
+// Dependency is key problem in software development
+//  at all scales (Kent Beck)
+
+// Design classes for easy change
+// Design classes for easy extention
+
+TEST_CASE("Class design: ", "[]") {}
